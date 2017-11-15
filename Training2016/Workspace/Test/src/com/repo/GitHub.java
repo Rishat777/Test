e46ad041c323fb8420f35a2e2ad06558ecc2bc2d
Rishat777 <rishatid7@gmail.com>
2017-11-15 13:53:20 -0500

    Success!

---
+++ Training2016/Workspace/Test/src/com/repo/GitHub.java
@@ -0,0 +1,12 @@
+package com.repo;
+
+public class GitHub {
+
+	public static void main(String[] args) {
+		log("Success");
+	}
+
+	public static void log (String text) {
+		System.out.println(text);
+	}
+}
