1d5e4ba01b86b051ee61cf18fae10f3893869bd4
Rishat777 <rishatid7@gmail.com>
2017-11-15 14:09:09 -0500

    Success

---
+++ Training2016/Workspace/repo/src/test/git.java
@@ -0,0 +1,14 @@
+package test;
+
+public class git {
+
+	public static void main(String[] args) {
+		log("Success");
+
+	}
+	
+	public static void log (String text) {
+		System.out.println(text);
+	}
+
+}
