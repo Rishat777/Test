12f0d658f10f1329137e2a2b3dea2d5396c6da7d
Rishat777 <rishatid7@gmail.com>
2017-11-15 14:17:28 -0500

    Updated

--- Training2016/Workspace/repo/src/test/git.java
+++ Training2016/Workspace/repo/src/test/git.java
@@ -1,14 +1,30 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class git {
 
 	public static void main(String[] args) {
 		log("Success");
-
+		change(2);
 	}
 	
 	public static void log (String text) {
 		System.out.println(text);
+	}
+	
+	public static void change (int num) {
+		int n = num;
+		if (n==2) {
+			ArrayList<String> arr = new ArrayList<>();
+			arr.add("Timati");
+			arr.add("Scriptonite");
+			arr.add("Almata");
+			Collections.sort(arr);
+			System.out.println("Names: " + arr);
+			
+		}
 	}
 
 }
